8653087733e6a111094ecf5fb44c546d48b1caac
Nichita Rares <nichita.rares@gmail.com>
2022-08-23 14:05:01 +0300

    Cleanup

--- FrontEnd/photo-gallery/src/app/services/photo.service.ts
+++ FrontEnd/photo-gallery/src/app/services/photo.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Storage } from '@capacitor/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +9,6 @@ export class PhotoService {
 
   public async addNewToGallery() {
     // Take a photo
-    const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
-    });
-    var imageUrl = capturedPhoto.webPath;
-    console.log(imageUrl)
+
   }
 }
